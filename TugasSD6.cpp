31881012a340c234d8e013970dec286052696e83
nadiravisa9-sys <nadiravisa9@gmail.com>
2026-04-22 21:51:27 +0700

    tugas perulangan 6

---
+++ TugasSD6.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	int total = 0;
+	
+	for ( int i = 10; i >= 2; i-= 2){
+		int jumlah = 0;
+		
+		for (int j = 10; j >= i; j -= 2)
+		cout << j;
+		jumlah += j;
+		
+		
+		if (j > i)
+		  cout << " = "
+	}
+}
